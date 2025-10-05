8388cc683caa465cfbc0ed7d3f5eed01dd9af670
brcheung <148270746+brcheungdev@users.noreply.github.com>
2025-10-05 17:36:53 +0900

    Create plot_graph_3-4.py

---
+++ notes/Python/src/plot_graph_3-4.py
@@ -0,0 +1,25 @@
+##################################################
+# plot_graph_3-4.py - plot graphs example 
+# for prime_list_3 and prime_list_4
+##################################################
+
+import matplotlib.pyplot as plt
+
+x   = [1000*i for i in range(11)]
+y_3 = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0] # ms
+y_4 = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] # ms
+
+fig = plt.figure(figsize=(6,4))
+ax = fig.add_subplot(1,1,1)
+
+ax.plot(x, y_3, label="prime_list_3" )
+ax.plot(x, y_4, label="prime_list_4")
+
+ax.grid()
+ax.set_title("prime_list_3 vs. prime_list_4")
+ax.set_xlabel("checked numbers")
+ax.set_ylabel("computation time (ms)")
+ax.legend()
+
+plt.savefig("primes_3-4.png")
+plt.show()
